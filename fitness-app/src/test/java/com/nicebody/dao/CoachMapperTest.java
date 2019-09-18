5c734b139bae30d925f680a60a393ef239888cc4
Git136314602 <136314602@qq.com>
2019-09-18 19:07:35 +0800

    hsq updateCoachService

--- fitness-app/src/test/java/com/nicebody/dao/CoachMapperTest.java
+++ fitness-app/src/test/java/com/nicebody/dao/CoachMapperTest.java
@@ -33,7 +33,7 @@ public class CoachMapperTest {
         CoachInfo coachInfo = new CoachInfo();
         coachInfo.setTagId(4);
         String value = OrderByUtil.coachConvent2String(OrderByEnum.PRICE.getCode());
-        List<CoachInfo> coachInfoList = coachMapper.queryCoachList(1, 2, coachInfo, value);
+        List<CoachInfo> coachInfoList = coachMapper.queryCoachList(0, 2, coachInfo, value);
         Assert.assertEquals(1, coachInfoList.size());
     }
 
@@ -52,56 +52,4 @@ public class CoachMapperTest {
         List<CoachImage> coachImageList = coachMapper.queryImageList(1);
         Assert.assertEquals(2,coachImageList.size());
     }
-    /*测试获取标签*/
-//    @Test
-//    public void queryTag(){
-//        List<Tag> coachTagList = coachTagMapper.queryCoachTag();
-//        System.out.println(coachTagList.get(1).getTagName());
-//        System.out.println(coachTagList.size());
-//    }
-    /*测试查找全部教练*/
-//    @Test
-//    public void queryAllCoach(){
-//        List<CoachInfo> coachInfoList = coachTagMapper.queryAllCoach();
-//        System.out.println(coachInfoList.size());
-//        System.out.println(coachInfoList.get(0).getCoachDesc());
-//        System.out.println(coachInfoList.get(1).getCoachName());
-//    }
-    /*测试模糊查询*/
-//    @Test
-//    public void queryCoachByName(){
-//        List<CoachInfo> coachInfoList = coachTagMapper.queryCoachByName("黄");
-//        System.out.println(coachInfoList.size());
-//        System.out.println(coachInfoList.get(0).getCoachName());
-//    }
-    /*测试根据Id查找*/
-//    @Test
-//    public void queryCoachById(){
-//        List<CoachInfo> coachInfoList = coachMapper.queryCoachById(2);
-//        System.out.println(coachInfoList.size());
-//        System.out.println(coachInfoList.get(0).getCoachName());
-//    }
-//    /*测试查找教练图片*/
-//    @Test
-//    public void  queryImage(){
-//        List<CoachImage> coachImages = coachMapper.queryAllImage(1);
-//        System.out.println(coachImages.size());
-//        System.out.println(coachImages.get(0).getImageUrl());
-//    }
-//    /*测试查找教练博客*/
-//    @Test
-//    public void queryCoachBlog(){
-//        List<CoachInfo> coachInfos = coachMapper.queryAllBlog(2);
-//        System.out.println(coachInfos.size());
-//        System.out.println(coachInfos.get(0).getUserBlogImage().getImageUrl());
-//    }
-
-    /*测试多条件分类查询*/
-//    @Test
-//    public void queryCoachTag(){
-//        List<CoachInfo> coachInfoList = coachTagMapper.queryCoachByTag(0,1);
-//        System.out.println(coachInfoList.size());
-//        System.out.println(coachInfoList.get(0).getCoachImage().getImageUrl());
-//    }
-
 }
--- fitness-app/src/test/java/com/nicebody/service/impl/CoachServiceImplTest.java
+++ fitness-app/src/test/java/com/nicebody/service/impl/CoachServiceImplTest.java
@@ -1,8 +1,18 @@
 package com.nicebody.service.impl;
 
+import com.nicebody.enums.OrderByEnum;
+import com.nicebody.pojo.CoachImage;
+import com.nicebody.pojo.CoachInfo;
+import com.nicebody.service.CoachService;
+import com.nicebody.util.OrderByUtil;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 /**
  * @ClassName CoachServiceImplTest
@@ -12,5 +22,35 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CoachServiceImplTest {
+    @Autowired
+    private CoachService coachService;
 
+    /** 多条件查找教练信息测试*/
+    @Test
+    public void getCoachListTest(){
+        CoachInfo coachInfo = new CoachInfo();
+        coachInfo.setTagId(4);
+        coachInfo.setCoachName("王");
+        String value = OrderByUtil.coachConvent2String(OrderByEnum.PRICE.getCode());
+        List<CoachInfo> coachInfoList = coachService.getCoachList(0,2,coachInfo,value);
+        Assert.assertEquals(1, coachInfoList.size());
+    }
+
+    /** 测试根据Id查找关注教练，以及教练个人信息*/
+    @Test
+    public void getCoachInfoTest(){
+        CoachInfo coachInfo = new CoachInfo();
+        List<CoachInfo> coachInfoList = coachService.getCoachInfo(0,1);
+        List<CoachInfo> coachInfoList1 = coachService.getCoachInfo(1,0);
+        Assert.assertEquals(1,coachInfoList.size());
+        Assert.assertEquals(2,coachInfoList1.size());
+    }
+
+    /** 测试查找教练图片*/
+    @Test
+    public void getImageListTest(){
+        CoachImage coachImage = new CoachImage();
+        List<CoachImage> coachImages = coachService.getImageList(1);
+        Assert.assertEquals(2,coachImages.size());
+    }
 }
